c78c20bff71b0413f7ac4d42504b09a67548b0b0
Paolo Antinori <pantinor@redhat.com>
2014-04-29 10:26:46 +0200

    Added comment to the helper plugin

--- build_with_go_offline.sh
+++ build_with_go_offline.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# This is an alternative build strategy to produce an offline Maven Repo.
+# It's not strictly needed since building at root level with just "mvn clean install" will produce an
+# offline maven repo using "karaf features plugin"
+
 MAVEN_OFFLINE_TMP=/tmp/mvn_offline
+
 rm -rf $MAVEN_OFFLINE_TMP
+
 mvn clean install \
     -DskipTests \
     -Pdependency-plugin \
